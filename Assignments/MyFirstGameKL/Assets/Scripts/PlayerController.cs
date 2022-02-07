ca6bfd3c26897320795d5967b65137e17b466fa3
Kayson Langford <10740041@my.uvu.edu>
2022-02-06 22:59:56 -0700

    first game prototype

---
+++ Assignments/MyFirstGameKL/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private float turnSpeed = 50.0f;
+    private float hInput;
+    private float fInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        fInput = Input.GetAxis("Vertical");
+        // Makes the player move 
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fInput);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * hInput);
+    }
+}
